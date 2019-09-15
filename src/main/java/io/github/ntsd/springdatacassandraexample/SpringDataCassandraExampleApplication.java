50cea94ef958f81369db37ddacd0325768a916be
Jirawat Boonkumnerd <jo06942@gmail.com>
2019-09-15 21:46:43 +0700

    Initial spring boot project

---
+++ src/main/java/io/github/ntsd/springdatacassandraexample/SpringDataCassandraExampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.ntsd.springdatacassandraexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataCassandraExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataCassandraExampleApplication.class, args);
+	}
+
+}
